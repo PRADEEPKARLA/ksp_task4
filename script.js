62fb3cc5818d245c39b0a5bb4f01acd8334e233a
PRADEEPKARLA <156448331+PRADEEPKARLA@users.noreply.github.com>
2024-01-23 13:28:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,154 @@
+console.log("hello world");
+//window.alert("welcome to webpage");
+
+
+//variables
+
+let variable1=4;
+let variable2=5;
+let variable3=6;
+console.log(variable2);
+console.log(variable3);
+
+
+let box1=300;
+let box2=200;
+let box3=100;
+console.log(box1);
+console.log(box3);
+
+let number1=30;
+number1=10; //here the number will be updated
+console.log(number1); //10
+
+//const number2=10;
+//number2=5; // the can't be assigned in case of const
+//console.log(number2); 
+
+//operators
+
+//arithmetic operators
+
+let num1=15;
+let num2=8;
+console.log(num1+num2); 
+console.log(num1-num2); 
+console.log(num1/num2); 
+console.log(num1%num2); 
+console.log(num1*num2); 
+console.log(num1**num2); 
+
+let number3=8;
+number3 +=2;    //number3=number3+2;
+result=number3-2;
+
+console.log(number3);
+console.log(result);
+
+
+let number4=10;
+number4++;           //post incerement
+console.log(number4);
+
+
+let number5=10;
+number5--;           //post decrement
+console.log(number5);
+
+let number6=10;
+++number6;           //pre increment
+console.log(number6);
+
+let number7=10;
+--number7;          //pre decrement
+console.log(number7);
+
+//assignment operator
+
+let number8=6;
+number8+=2;          //opreator +=
+//result=number+2;
+console.log(number8);
+//console.log(result);
+
+let number9=6;
+number9-=2;          //opreator -=
+//result=number-2;
+console.log(number9);
+//console.log(result);
+
+
+let num3=6;
+num3*=2;             //opreator *=
+//result=number*2;
+console.log(num3);
+//console.log(result);
+
+let num4=6;
+num4**=2;             //opreator **=
+//result=number**2;
+console.log(num4);
+//console.log(result);
+
+
+let num5=6;
+num5/=2;               //opreator /=
+//result=number/2;
+console.log(num5);
+//console.log(result);
+
+let num6=6;
+num6%=2;              //opreator %=
+//result=number*2;
+console.log(num6);
+//console.log(result);
+
+//comparision operators
+
+let num7=10;
+let num8=8;
+console.log(num7==num8);
+console.log(num7>num8);
+console.log(num7<num8);
+console.log(num7===num8);
+console.log(!(num7===num8));
+
+
+let mahi=3;
+let yuvi=5;
+console.log(mahi<=yuvi);
+console.log(mahi>=yuvi);
+console.log(mahi>3);
+
+
+//logical operators
+
+let firstNum1=6;
+let firstNum2="6";
+console.log(firstNum1==firstNum2);
+console.log(firstNum1===firstNum2);
+console.log(firstNum1>2 && firstNum2>6);
+console.log(firstNum1>2 ||  firstNum2>6);
+console.log(!(firstNum1>2 ||  firstNum2>6));
+console.log(firstNum1==6 ||  firstNum2==6);
+console.log(firstNum1===6 ||  firstNum2===6);
+console.log(firstNum1===6 &&  firstNum2===6);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
